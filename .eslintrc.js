715584a106c556037acd9c6ae0da80ea2e8ea20c
Chris Chu <op.github.io@gmail.com>
2020-12-24 09:36:16 +0800

    Project configuration files

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,11 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+  ],
   extends: [
+    'airbnb-typescript',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
@@ -20,5 +23,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ['error'],
   },
 };
